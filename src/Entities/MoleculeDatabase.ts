3bd9d1da1fd00b91d974e85292a9d4c73a70df8a
lberanger <louis.beranger@etu.univ-lyon1.fr>
2020-02-13 10:54:20 +0100

    Delete cache when updating db

--- src/Entities/MoleculeDatabase.ts
+++ src/Entities/MoleculeDatabase.ts
@@ -1,6 +1,8 @@
 import { Molecule } from "./entities";
 import AbstractDatabase from "./AbstractDatabase";
 import MoleculeVersionTree from "./MoleculeVersionTree";
+import nano = require("nano");
+import SearchWorker from "../search_worker";
 
 export default class MoleculeDatabase extends AbstractDatabase<Molecule> {
   /**
@@ -14,5 +16,39 @@ export default class MoleculeDatabase extends AbstractDatabase<Molecule> {
     if (mols.length) {
       return new MoleculeVersionTree(mols);
     }
+  }
+
+  /** Bulk create */
+  bulkCreate(docs: Molecule[]) {
+    SearchWorker.clearCache();
+    return super.bulkCreate(docs);
+  }
+
+  /** Bulk update */
+  bulkUpdate(docs: Molecule[]) {
+    SearchWorker.clearCache();
+    return super.bulkUpdate(docs);
+  }
+
+  /** Bulk delete */
+  bulkDelete(documents: Molecule[]) {
+    SearchWorker.clearCache();
+    return super.bulkDelete(documents);
+  }
+
+  /** Bulk create, update or delete */
+  bulk(docs: nano.BulkModifyDocsWrapper) {
+    SearchWorker.clearCache();
+    return super.bulk(docs);
+  }
+
+  save(element: Molecule) {
+    SearchWorker.clearCache();
+    return super.save(element);
+  }
+
+  delete(element: Molecule) {
+    SearchWorker.clearCache();
+    return super.delete(element);
   }
 }
