12c5442f9c34135b8c973471f91156c386b88226
Cecile Hilpert <cecile.hilpert@gmail.com>
2021-03-10 12:02:51 +0100

    add log with cmd line

--- utils/create_conect_pdb_coreScript.sh
+++ utils/create_conect_pdb_coreScript.sh
@@ -37,19 +37,23 @@ then
   gro_box="$pdb"
 else
   # Create the box
+  echo Create box : gmx editconf -f "$pdb" -o "$gro_box" -box 15 15 18 -noc
   gmx editconf -f "$pdb" -o "$gro_box" -box 15 15 18 -noc  >editconf.stdout 2>editconf.stderr
 fi
 
 # Create the computed topology .tpr
+echo Create tpr : gmx grompp -f "$mdp" -c "$gro_box" -p "$top" -o "$tpr_run"
 gmx grompp -f "$mdp" -c "$gro_box" -p "$top" -o "$tpr_run" >grompp.stdout 2>grompp.stderr
 
 if [ $DEL_WATER_BOOL == "YES" ]
 then
+  echo Delete water (1) : gmx make_ndx -f "$gro_box" -o "$index_ndx"
   # File to give on stdin to make_ndx
   printf '!"W"\nq\n' > $tmp_stdin
   # Create index with a category without W
   gmx make_ndx -f "$gro_box" -o "$index_ndx" < $tmp_stdin >make_ndx.stdout 2>make_ndx.stderr
 
+  echo Delete water (2) : gmx trjconv -n "$index_ndx" -s "$tpr_run" -f "$gro_box" -o "$output_conect_no_water" -conect
   # File to give on stdin to trjconv
   printf '!W\n' > $tmp_stdin
   # Create the PDB with conect entries without water 
@@ -61,6 +65,7 @@ fi
 # File to give on stdin to trjconv
 printf '0\n' > $tmp_stdin
 # Create the PDB with conect entries with water 
+echo Create pdb with conect entries : gmx trjconv -s "$tpr_run" -f "$gro_box" -o "$output_conect" -conect
 gmx trjconv -s "$tpr_run" -f "$gro_box" -o "$output_conect" -conect < $tmp_stdin  >trjconv2.stdout 2>trjconv2.stderr
 
 echo "File $output_conect has been written."
