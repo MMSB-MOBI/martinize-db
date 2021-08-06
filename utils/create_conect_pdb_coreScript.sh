a4ab724d2cf8c49fadf34cb9a9fd69476483abaa
Cecile Hilpert <cecile.hilpert@gmail.com>
2021-08-06 15:32:42 +0200

    create conect pdb log details

--- utils/create_conect_pdb_coreScript.sh
+++ utils/create_conect_pdb_coreScript.sh
@@ -38,12 +38,12 @@ then
 else
   # Create the box
   echo Create box : gmx editconf -f "$pdb" -o "$gro_box" -box 15 15 18 -noc
-  gmx editconf -f "$pdb" -o "$gro_box" -box 15 15 18 -noc  >editconf.stdout 2>editconf.stderr
+  gmx editconf -f "$pdb" -o "$gro_box" -box 15 15 18 -noc  > 1.editconf.stdout 2> 1.editconf.stderr
 fi
 
 # Create the computed topology .tpr
 echo Create tpr : gmx grompp -f "$mdp" -c "$gro_box" -p "$top" -o "$tpr_run"
-gmx grompp -f "$mdp" -c "$gro_box" -p "$top" -o "$tpr_run" >grompp.stdout 2>grompp.stderr
+gmx grompp -f "$mdp" -c "$gro_box" -p "$top" -o "$tpr_run" > 2.grompp.stdout 2> 2.grompp.stderr
 
 if [ $DEL_WATER_BOOL == "YES" ]
 then
@@ -51,13 +51,13 @@ then
   # File to give on stdin to make_ndx
   printf '!"W"\nq\n' > $tmp_stdin
   # Create index with a category without W
-  gmx make_ndx -f "$gro_box" -o "$index_ndx" < $tmp_stdin >make_ndx.stdout 2>make_ndx.stderr
+  gmx make_ndx -f "$gro_box" -o "$index_ndx" < $tmp_stdin > 3.make_ndx.stdout 2> 3.make_ndx.stderr
 
   echo "Delete water (2)" : gmx trjconv -n "$index_ndx" -s "$tpr_run" -f "$gro_box" -o "$output_conect_no_water" -conect
   # File to give on stdin to trjconv
   printf '!W\n' > $tmp_stdin
   # Create the PDB with conect entries without water 
-  gmx trjconv -n "$index_ndx" -s "$tpr_run" -f "$gro_box" -o "$output_conect_no_water" -conect < $tmp_stdin >trjconv.stdout 2>trjconv.stderr
+  gmx trjconv -n "$index_ndx" -s "$tpr_run" -f "$gro_box" -o "$output_conect_no_water" -conect < $tmp_stdin > 4.trjconv.stdout 2> 4.trjconv.stderr
 
   echo "File $output_conect_no_water has been written."
 fi
@@ -66,6 +66,6 @@ fi
 printf '0\n' > $tmp_stdin
 # Create the PDB with conect entries with water 
 echo Create pdb with conect entries : gmx trjconv -s "$tpr_run" -f "$gro_box" -o "$output_conect" -conect
-gmx trjconv -s "$tpr_run" -f "$gro_box" -o "$output_conect" -conect < $tmp_stdin  >trjconv2.stdout 2>trjconv2.stderr
+gmx trjconv -s "$tpr_run" -f "$gro_box" -o "$output_conect" -conect < $tmp_stdin  > 5.trjconv.stdout 2> 5.trjconv.stderr
 
 echo "File $output_conect has been written."
