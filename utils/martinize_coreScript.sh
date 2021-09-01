d7a6a96462e90fb23b61f2a8d6d8134a763959e2
glaunay <pitooon@gmail.com>
2021-09-01 14:58:28 +0200

    filtered nan coordinates from martinized record

--- utils/martinize_coreScript.sh
+++ utils/martinize_coreScript.sh
@@ -8,3 +8,11 @@
 cd $basedir
 martinize2_path="martinize2"
 $martinize2_path $martinizeArgs -maxwarn 9999
+
+# in case of missing all atoms coordinates in PDB files beads with nan coordinates are dumped
+# which makes subsequent insane calls crash. (eq: 1PT4)
+if [[ -e output.pdb ]]
+then
+        mv output.pdb output.pdb.may_nan
+        grep -v 'nan     nan     nan' output.pdb.may_nan > output.pdb
+fi
