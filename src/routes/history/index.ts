01b3b8541a3957fac4969ed3550f3238ead1402b
Cecile Hilpert <cecile.hilpert@gmail.com>
2021-10-27 11:41:23 +0200

    update route

--- src/routes/history/index.ts
+++ src/routes/history/index.ts
@@ -2,12 +2,14 @@ import { Router } from 'express';
 import ListHistoryRouter from './list'; 
 import GetHistoryRouter from './get'
 import DeleteHistoryRouter from './delete'
+import UpdateHistoryRouter from './update'
 
 const HistoryRouter = Router();
 
 HistoryRouter.use('/list', ListHistoryRouter); 
 HistoryRouter.use('/get', GetHistoryRouter); 
 HistoryRouter.use('/delete', DeleteHistoryRouter); 
+HistoryRouter.use('/update', UpdateHistoryRouter); 
 
 export default HistoryRouter; 
 
